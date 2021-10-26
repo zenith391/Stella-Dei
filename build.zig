d9f1ab6e8f76af8deb1015bf31bf5178b0f386eb
zenith391 <39484230+zenith391@users.noreply.github.com>
2021-10-26 18:05:06 +0200

    Converter step in build.zig
    This allows to write the source code with tabs

--- build.zig
+++ build.zig
@@ -1,5 +1,66 @@
 const std = @import("std");
 
+/// Step used to convert from tabs to space
+const ConvertStep = struct {
+    generated_file: std.build.GeneratedFile,
+    step: std.build.Step,
+    builder: *std.build.Builder,
+    root: []const u8,
+
+    pub fn create(builder: *std.build.Builder, root: []const u8) *ConvertStep {
+        const self = builder.allocator.create(ConvertStep) catch unreachable;
+        self.* = .{
+            .generated_file = undefined,
+            .step = std.build.Step.init(.install_file, "convert", builder.allocator, ConvertStep.make),
+            .builder = builder,
+            .root = root
+        };
+
+        self.generated_file = .{ .step = &self.step };
+        return self;
+    }
+
+    pub fn getSource(self: ConvertStep) std.build.FileSource {
+        return .{ .generated = &self.generated_file };
+    }
+
+    pub fn make(step: *std.build.Step) !void {
+        const self = @fieldParentPtr(ConvertStep, "step", step);
+
+        var sourceDir = try std.fs.cwd().openDir(std.fs.path.dirname(self.root).?, .{ .iterate = true });
+        defer sourceDir.close();
+
+        var cacheRoot = try std.fs.cwd().openDir(self.builder.cache_root, .{});
+        defer cacheRoot.close();
+
+        var targetDir = try cacheRoot.makeOpenPath("converted", .{});
+        defer targetDir.close();
+
+        var walker = try sourceDir.walk(self.builder.allocator);
+        while (try walker.next()) |entry| {
+            var source = try sourceDir.openFile(entry.path, .{});
+            defer source.close();
+
+            const text = try source.readToEndAlloc(self.builder.allocator, std.math.maxInt(usize));
+            defer self.builder.allocator.free(text);
+
+            // Replace every occurence of a tab by a single space
+            _ = std.mem.replace(u8, text, "\t", " ", text);
+
+            // Ensure the target file's parent directory exists
+            const dirname = std.fs.path.dirname(entry.path) orelse ".";
+            try targetDir.makePath(dirname);
+
+            var target = try targetDir.createFile(entry.path, .{});
+            defer target.close();
+            try target.writeAll(text);
+        }
+
+        self.generated_file.path = try std.mem.concat(self.builder.allocator, u8,
+            &[_][]const u8 { self.builder.cache_root, "/converted/main.zig" });
+    }
+};
+
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
@@ -11,12 +72,15 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("Name-Not-Included", "src/main.zig");
+    const convert = ConvertStep.create(b, "src/main.zig");
+
+    const exe = b.addExecutableSource("name-not-included", convert.getSource());
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
 
     const run_cmd = exe.run();
+
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.log.info("All your codebase are belong to us.", .{});
+	std.log.info("All your codebase are belong to us.", .{});
 }
 
 test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+	try std.testing.expectEqual(10, 3 + 7);
 }
