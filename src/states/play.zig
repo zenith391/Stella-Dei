24bbe6430c386fe20324aabb86fd5a69956fa791
zenith391 <39484230+zenith391@users.noreply.github.com>
2022-02-08 20:06:37 +0100

    Working icosphere

--- src/states/play.zig
+++ src/states/play.zig
@@ -29,15 +29,93 @@ const icoVertices = &[_]f32 {
 	-icoZ, -icoX, 0,
 };
 
-const icoIndices = &[_]u32 {
+const icoIndices = &[_]gl.GLuint {
 	0, 4, 1, 0, 9, 4, 9, 5, 4, 4, 5, 8, 4, 8, 1, 8, 10, 1, 8, 3, 10, 5, 3, 8,
 	5, 2, 3, 2, 7, 3, 7, 10, 3, 7, 6, 10, 7, 11, 6, 11, 0, 6, 0, 1, 6,
 	6, 1, 10, 9, 0, 11, 9, 11, 2, 9, 2, 5, 7, 2, 11
 };
 
+const IndexPair = struct {
+	first: gl.GLuint,
+	second: gl.GLuint
+};
+
 pub const Planet = struct {
 	vao: gl.GLuint,
 	numTriangles: gl.GLint,
+
+	const LookupMap = std.AutoHashMap(IndexPair, gl.GLuint);
+	fn vertexForEdge(lookup: *LookupMap, vertices: *std.ArrayList(f32), first: gl.GLuint, second: gl.GLuint) !gl.GLuint {
+		const a = if (first > second) first  else second;
+		const b = if (first > second) second else first;
+
+		const pair = IndexPair { .first = a, .second = b };
+		const result = try lookup.getOrPut(pair);
+		if (!result.found_existing) {
+			result.value_ptr.* = @intCast(gl.GLuint, vertices.items.len / 3);
+			const edge0 = Vec3.new(
+				vertices.items[a*3+0],
+				vertices.items[a*3+1],
+				vertices.items[a*3+2],
+			);
+			const edge1 = Vec3.new(
+				vertices.items[b*3+0],
+				vertices.items[b*3+1],
+				vertices.items[b*3+2],
+			);
+			const point = edge0.add(edge1).norm();
+			try vertices.append(point.x());
+			try vertices.append(point.y());
+			try vertices.append(point.z());
+		}
+
+		return result.value_ptr.*;
+	}
+
+	const IndexedMesh = struct {
+		vertices: []f32,
+		indices: []gl.GLuint
+	};
+
+	fn subdivide(allocator: std.mem.Allocator, vertices: []const f32, indices: []const gl.GLuint) !IndexedMesh {
+		var lookup = LookupMap.init(allocator);
+		defer lookup.deinit();
+		var result = std.ArrayList(gl.GLuint).init(allocator);
+		var verticesList = std.ArrayList(f32).init(allocator);
+		try verticesList.appendSlice(vertices);
+
+		var i: usize = 0;
+		while (i < indices.len) : (i += 3) {
+			var mid: [3]gl.GLuint = undefined;
+			var edge: usize = 0;
+			while (edge < 3) : (edge += 1) {
+				mid[edge] = try vertexForEdge(&lookup, &verticesList,
+					indices[i+edge], indices[i+(edge+1)%3]);
+			}
+
+			try result.ensureUnusedCapacity(12);
+			result.appendAssumeCapacity(indices[i+0]);
+			result.appendAssumeCapacity(mid[0]);
+			result.appendAssumeCapacity(mid[2]);
+
+			result.appendAssumeCapacity(indices[i+1]);
+			result.appendAssumeCapacity(mid[1]);
+			result.appendAssumeCapacity(mid[0]);
+
+			result.appendAssumeCapacity(indices[i+2]);
+			result.appendAssumeCapacity(mid[2]);
+			result.appendAssumeCapacity(mid[1]);
+
+			result.appendAssumeCapacity(mid[0]);
+			result.appendAssumeCapacity(mid[1]);
+			result.appendAssumeCapacity(mid[2]);
+		}
+
+		return IndexedMesh {
+			.vertices = verticesList.toOwnedSlice(),
+			.indices = result.toOwnedSlice(),
+		};
+	}
 
 	pub fn generate(allocator: std.mem.Allocator, numSubdivisions: usize) !Planet {
 		_ = allocator;
@@ -54,13 +132,17 @@ pub const Planet = struct {
 		gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
 		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo);
 
-		gl.bufferData(gl.ARRAY_BUFFER, @intCast(isize, icoVertices.len * @sizeOf(f32)), icoVertices, gl.STATIC_DRAW);
-		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, @intCast(isize, icoIndices.len * @sizeOf(f32)), icoIndices, gl.STATIC_DRAW);
+		const subdivided = try subdivide(allocator, icoVertices, icoIndices);
+
+
+		gl.bufferData(gl.ARRAY_BUFFER, @intCast(isize, subdivided.vertices.len * @sizeOf(f32)), subdivided.vertices.ptr, gl.STATIC_DRAW);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, @intCast(isize, subdivided.indices.len * @sizeOf(f32)), subdivided.indices.ptr, gl.STATIC_DRAW);
 		gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 3 * @sizeOf(f32), null);
 		gl.enableVertexAttribArray(0);
+
 		return Planet {
 			.vao = vao,
-			.numTriangles = icoIndices.len
+			.numTriangles = @intCast(gl.GLint, subdivided.indices.len)
 		};
 	}
 
